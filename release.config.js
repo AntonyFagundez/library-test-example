a928086de89e266764477348777342bb15e82efb
Antony Fagundez <jhonfagundez@gmail.com>
2023-04-19 14:02:26 -0300

    ci: add semantic-release

---
+++ release.config.js
@@ -0,0 +1,33 @@
+/**
+ * @type {import("semantic-release")}
+ */
+module.exports = {
+  pkgRoot: "dist",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md",
+      },
+    ],
+    "@semantic-release/npm",
+    "@semantic-release/github",
+    [
+      "@semantic-release/exec",
+      {
+        // This format is required by semantic release
+        // eslint-disable-next-line no-template-curly-in-string
+        prepareCmd: "npx rpj package.json version ${nextRelease.version}",
+      },
+    ],
+    [
+      "@semantic-release/git",
+      {
+        message: "Release <%= nextRelease.version %> [skip ci]",
+        assets: ["package.json", "CHANGELOG"],
+      },
+    ],
+  ],
+};
