93efbaeeabe4255cad6ce382d1124a3d50bbae0a
Antony Fagundez <jhonfagundez@gmail.com>
2023-04-19 14:02:39 -0300

    docs: add a11y addon

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,6 +5,7 @@ const config: StorybookConfig = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/addon-a11y"
   ],
   framework: {
     name: "@storybook/react-webpack5",
@@ -13,5 +14,24 @@ const config: StorybookConfig = {
   docs: {
     autodocs: "tag",
   },
+  features: {
+    // to enable composition
+    buildStoriesJson: true
+  },
+  webpackFinal: (config, _) => {
+    config.optimization = {
+      minimize: false,
+      minimizer: [],
+      splitChunks: {
+        chunks: "all",
+        minSize: 30 * 1024, // 30KB
+        maxSize: 1024 * 1024, // 1MB
+      }
+    };
+
+    return {
+      ...config,
+    }
+  }
 };
 export default config;
